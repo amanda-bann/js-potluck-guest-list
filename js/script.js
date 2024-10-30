d5fd137e7bc4a8c04db4035df3ab6ce611095229
amanda-bann <amanda_klap@protonmail.com>
2024-10-30 16:47:41 -0600

    Refactor code

--- js/script.js
+++ js/script.js
@@ -15,9 +15,7 @@ addGuestButton.addEventListener("click", function (){
     const guest = guestInput.value;
    // console.log(guest);
    if (guest !== "") {
-       const listItem = document.createElement("li");
-       listItem.innerText = guest;
-       guestList.append(listItem);
+       addToList(guest);
        clearInput ();
    }
 });
@@ -26,6 +24,10 @@ const clearInput = function () {
     guestInput.value = "";
 };
 
-
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+       listItem.innerText = guest;
+       guestList.append(listItem);
+};
 
                                    
